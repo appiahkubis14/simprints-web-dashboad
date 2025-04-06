b52ff9796b33a0bf889eeac63ba32df04f62fefc
ernest opoku-kwarteng <eopokukwarteng@gmail.com>
2025-04-06 19:44:54 +0000

    update

--- simprintspc/settings.py
+++ simprintspc/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-x@@*u3@k^9xhw+t0#t#$_o$)f^8ko-#+owek1fk(=azvb0m=(0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
